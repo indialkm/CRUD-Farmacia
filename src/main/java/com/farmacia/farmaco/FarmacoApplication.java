afc1f2aab931a659e07828027135b81b17917467
Ingrid Alkimim <ingridalkm@outlook.com>
2024-06-11 08:36:35 -0300

    primeiroCommit

---
+++ src/main/java/com/farmacia/farmaco/FarmacoApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmacia/farmaco/FarmacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
